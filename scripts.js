8ef8872eaa5e6a3bc7bde90e01df7e62122ef497
Ryan B <ryan.bliton@gmail.com>
2016-04-09 12:48:38 -0500

    exercise complete

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var hohohoElement = document.getElementById("reindeer");
+
+for (i=0;i<reindeer.length;i++) {
+    hohohoElement.innerHTML += "<li>" +colors[i] +" " +reindeer[i] +"</li>";
+}
+
